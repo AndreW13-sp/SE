ac83c18866048ab827aad340beee62df05a7ee1f
AndreW13-sp <panchalswaraj13@gmail.com>
2022-08-01 20:44:12 +0530

    updated

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from pathlib import Path
 from flask import Flask, request, render_template, send_file
 from gtts import gTTS
+from flask_cors import CORS, cross_origin
 import speech_recognition as sr
 
 # Application Settings
 app = Flask(__name__)
+CORS(app, support_credentials=True)
+app.config['CORS_HEADERS']='application/json'
 file_name = "mic.wav"
 
 # API Routes
 @app.route("/", methods=["GET", "POST"])
+@cross_origin(support_credentials=True)
 def index():
    if request.method == "POST":
       data = request.get_json()
-      myobj = gTTS(text=user_text, lang="en", slow=False)
+      myobj = gTTS(text=data.get('text'), lang="en", slow=False)
       myobj.save(file_name)
       return send_file(Path(file_name), mimetype="audio/wav", as_attachment=True, download_name=file_name)
    return render_template("home.html")
@@ -31,7 +35,7 @@ def handle_upload():
             converted_text = recognizer.recognize_google(audio_text, key=None)
             print(f"My Text: {converted_text}")
             return { "transcribed_speech": "converted_text" }
-         except e:
+         except Exception as e:
             return { "error": "Something went wrong when trying to convert stt" }
    else:
       return { "error": "Unsupported action type!" }
