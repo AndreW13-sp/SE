73fd817eb79c294736c8be14bffcd89e757474ff
Ayush Dedhia <ayushdedhia25@gmail.com>
2022-08-01 17:45:31 +0530

    Implemented Text-to-speech

--- main.py
+++ main.py
@@ -1,26 +1,45 @@
-from cgitb import text
+from pathlib import Path
 from flask import Flask, request, render_template, send_file
 from gtts import gTTS
-from flask_cors import CORS, cross_origin
+import speech_recognition as sr
 
-import os
-
-
+# Application Settings
 app = Flask(__name__)
-CORS(app, support_credentials=True)
-app.config['CORS_HEADERS'] = 'application/json'
+file_name = "mic.wav"
 
-
-@app.route('/', methods=['GET','POST'])
-@cross_origin(supports_credentials=True)
+# API Routes
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
-        myvariable=request.json['text']
-        myobj = gTTS(text= myvariable, lang='en', slow=False)
-        myobj.save('mic.mp3')
-        return send_file('mic.mp3',as_attachment=True)
-    return render_template('home.html')
+   if request.method == "POST":
+      data = request.get_json()
+      myobj = gTTS(text=user_text, lang="en", slow=False)
+      myobj.save(file_name)
+      return send_file(Path(file_name), mimetype="audio/wav", as_attachment=True, download_name=file_name)
+   return render_template("home.html")
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/upload", methods=["POST"])
+def handle_upload():
+   file = request.files.get("speech_file")
+   action = request.form.get("action")
+   
+   if action == "SpeechToText":
+      recognizer = sr.Recognizer()
+      with sr.AudioFile("./mic.wav") as source:
+         audio_text = recognizer.record(source)
+         try:
+            converted_text = recognizer.recognize_google(audio_text, key=None)
+            print(f"My Text: {converted_text}")
+            return { "transcribed_speech": "converted_text" }
+         except e:
+            return { "error": "Something went wrong when trying to convert stt" }
+   else:
+      return { "error": "Unsupported action type!" }
+   
+   return { "foo": "bar" }
+
+# ----------------------------------------------------------------------------
+# Running main server here...
+# ----------------------------------------------------------------------------
+if __name__ == "__main__":
+   app.run(debug=True)
